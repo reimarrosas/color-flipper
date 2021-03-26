374c6162d8354e0fba1552015eb85b20ffc1b561
Reimar Rosas <reimarrosas0208@gmail.com>
2021-03-26 11:15:00 -0400

    Added more inputs for RGB aside from hex, and an output box

---
+++ index.js
@@ -0,0 +1,18 @@
+/**
+ * Take Input
+ *    selection?
+ *        if hex, one input
+ *        else three
+ *    on form submit
+ *        add event listener
+ *    validate
+ *      check if #ffffff for HEX and if less than 256
+ *      and greater or equal to zero on RGB
+ *    save
+ * Calculate
+ *    take results from input
+ *    calculate depending on format (RGB/HEX)
+ *    save
+ * Output
+ *    display output to copy-able input:disabled
+ */
