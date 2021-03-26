267ef6cca46e395dc30f6e6fc888e3bdf359c691
Reimar Rosas <reimarrosas0208@gmail.com>
2021-03-26 12:09:25 -0400

    Implemented a working prototype for the take input task

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 /**
  * Take Input
- *    selection?
+ *    selection? !!
  *        if hex, one input
  *        else three
  *    on form submit
@@ -16,3 +16,47 @@
  * Output
  *    display output to copy-able input:disabled
  */
+
+const colorOptions = document.getElementById("color-type");
+colorOptions.addEventListener("change", toggleFormInputs);
+
+function toggleFormInputs(e) {
+  const colorHex = document.getElementById("color-hex");
+  const colorRgb = document.getElementById("color-rgb");
+
+  const decideDisplay = compareSelectedToDisplayedInput(e.target.value);
+  colorHex.style.display = decideDisplay(colorHex);
+  colorRgb.style.display = decideDisplay(colorRgb);
+}
+
+function compareSelectedToDisplayedInput(colorType) {
+  return (colorTextInputs) => {
+    return colorTextInputs.id.includes(colorType)
+                ? "block"
+                : "none";
+  };
+}
+
+const formInput = document.getElementById("user-input");
+formInput.addEventListener("submit", (e) => {handleUserInput(e, colorOptions.value)});
+
+const inputList = [];
+function handleUserInput(e, colorType) {
+  const inputElements = filterVisibleInputs([...e.target.elements], colorType);
+  inputElements.forEach(input => {
+    if (validateInput(input, colorType)) {
+      inputList.push(input)
+    } else alert("Invalid Input!");
+  });
+  e.preventDefault();
+}
+
+function filterVisibleInputs(inputList, colorType) {
+  return inputList.filter(input => input.name.includes(colorType));
+}
+
+function validateInput(input, colorType) {
+  return colorType === "hex"
+          ? /#[0-9a-f]/.test(input)
+          : /[0-255]/.test(input);
+}
